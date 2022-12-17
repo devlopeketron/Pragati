b513b9e08282e51f73d9cccd552d314e19927b9d
devlopeketron <ketronmadankar@gmail.com>
2022-12-17 20:00:00 +0530

    firstdd commitcc

--- src/App.js
+++ src/App.js
@@ -6,29 +6,72 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      name: 'aniket',
-    };
+      // monster:[
+      //   {
+      //     name:'linda'
+      //   },
+      //   {
+      //     name:'franky'
+      //   },
+      //   {
+      //     name:'jacky'
+      //   },{
+      //     name:'andrei'
+      //   }
+
+      // ]
+
+      // monster1:{
+      //   name:'linda'
+      // },
+      // monster2:{
+      //   name:'frank'
+      // },
+      // monster3:{
+      //   name:'jacky'
+      // }
+      //name: 'aniket',
+    monster: [],
+     };
+     console.log(constructor , '-----------');
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((Response) => 
+    Response.json()).then((user)=>
+    this.setState(() => {
+      return {monster :user}
+    
+    },()=>{
+      console.log(this.state)
+    }
+    ))
   }
    
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HI {this.state.name}
-        </p>
-        <button
-         onClick={() => {
-          this.setState({name :  'Pragati'});
-
-         }}> Change buttongggg</button>
- 
+      <input className='search-box' type='search' placeholder='search monster' onChange = {(events)=>{
+      console.log(events.target.value);
+      const searchstring = events.target.value.toLocaleLowerCase();
+      const filteredMonster= this.state.monster.filter((monster) =>{
+        return monster.name.toLocaleLowerCase().includes(searchstring);
+      });
+      this.setState(()=> {
+        return { monster : filteredMonster};
+      });
+      }}
+       />
+    
+         {
+          this.state.monster.map((monster)=>{
+            return<h1>{monster.name}</h1>
+          })}
+       
+          
 
         
          
     
-      </header>
     </div>
   );
 }
